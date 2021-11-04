80eaa9344dec92e210772df9f570d33b099f70f8
Nicole Pingar <nicolepingar@yahoo.com>
2021-11-04 09:55:13 -0400

    changes styles.css

--- public/service-worker.js
+++ public/service-worker.js
@@ -2,14 +2,14 @@ const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 const FILES_TO_CACHE = [
-    './',
-    './index.html',
-    './db.js',
-    './index.js',
-    './manifest.webmanifest',
-    './style.css',
-    './icons/icon-192x192.png',
-    './icons/icon-512x512.png',
+    '/',
+    '/index.html',
+    '/db.js',
+    '/index.js',
+    '/manifest.webmanifest',
+    '/styles.css',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
 ];
 
 // install
@@ -22,7 +22,7 @@ self.addEventListener("install", function (evt) {
     // pre cache all static assets
     evt.waitUntil(
         caches.open(CACHE_NAME).then((cache) => {
-            cache.addAll(FILES_TO_CACHE)
+            return cache.addAll(FILES_TO_CACHE)
         }) // !! error here
     );
 
